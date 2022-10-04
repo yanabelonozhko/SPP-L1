6507a984e7658e86403f2bfb2efe694796d0bf59
Yana <96658718+yanabelonozhko@users.noreply.github.com>
2022-10-04 23:34:54 +0300

    files add

---
+++ ConsoleApp1/SerializationJson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace lab1
+{
+    public class SerializationJson:ISerialization
+    {
+        TraceResult _result;
+        
+        public SerializationJson(TraceResult result) 
+        {
+            _result = result;
+        }
+        public void ShowResult(Stream stream) 
+        {
+            string serializedResult = JsonConvert.SerializeObject(_result, Formatting.Indented);
+            byte[] buffer = Encoding.Default.GetBytes(serializedResult);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ ConsoleApp1/SerializationXML.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace lab1
+{
+    public class SerializationXML : ISerialization
+    {
+        TraceResult _result;
+        public SerializationXML(TraceResult result) 
+        {
+            _result = result;
+        }  
+        public void ShowResult(Stream stream)
+        {
+            string serializedResult;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(TraceResult),
+            new Type[] { typeof(MethodResult), typeof(ThreadResult) });
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, _result);
+                serializedResult = textWriter.ToString();
+            }
+            byte[] buffer = Encoding.Default.GetBytes(serializedResult);
+            stream.Write(buffer, 0, buffer.Length);
+        } 
+    }
+}
